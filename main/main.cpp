5069eaee40c00d31114f9fd030a9db326d52dfcb
wachinaba <wachinaba@gmail.com>
2023-07-31 21:36:44 +0900

    add project files

---
+++ main/main.cpp
@@ -0,0 +1,16 @@
+#include "Arduino.h"
+
+void setup()
+{
+  Serial.begin(115200);
+  while (!Serial)
+  {
+    ; // wait for serial port to connect
+  }
+}
+
+void loop()
+{
+  Serial.println("loop");
+  delay(1000);
+}
